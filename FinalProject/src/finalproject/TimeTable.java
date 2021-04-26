5283a382f532e029a8e437be1ed1490bb2ded8b3
Beto <evol999@hotmail.com>
2021-04-26 18:11:39 -0500

    Added variables for TimeTable class

--- FinalProject/src/finalproject/TimeTable.java
+++ FinalProject/src/finalproject/TimeTable.java
@@ -5,10 +5,32 @@
  */
 package finalproject;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author evol9
  */
 public class TimeTable {
+
+    /**
+     * @return the tsList
+     */
+    public ArrayList<TimeSlot> getTsList() {
+        return tsList;
+    }
+
+    /**
+     * @param tsList the tsList to set
+     */
+    public void setTsList(ArrayList<TimeSlot> tsList) {
+        this.tsList = tsList;
+    }
+
+    public TimeTable(ArrayList<TimeSlot> tsList) {
+        this.tsList = tsList;
+    }
+    private ArrayList<TimeSlot> tsList;
+    
     
 }
