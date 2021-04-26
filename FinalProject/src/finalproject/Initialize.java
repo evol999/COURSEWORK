f35baf8b14991b43767deb09f20866a55f3619da
Beto <evol999@hotmail.com>
2021-04-26 17:29:47 -0500

    Added new class to handle initialization.

---
+++ FinalProject/src/finalproject/Initialize.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package finalproject;
+
+/**
+ *
+ * @author evol9
+ */
+public class Initialize {
+
+    public Initialize() {
+        System.out.println("Hello from Init");
+    }
+    
+}
