cc34ac835b4ec4273a11ab0c04be7fe939847f2c
Beto <evol999@hotmail.com>
2021-04-26 20:06:23 -0500

    Initialize class updated

--- FinalProject/src/finalproject/Initialize.java
+++ FinalProject/src/finalproject/Initialize.java
@@ -5,14 +5,28 @@
  */
 package finalproject;
 
+import java.io.InputStream;
+
 /**
  *
  * @author evol9
  */
 public class Initialize {
+    boolean initOK;
 
     public Initialize() {
+        this.initOK = false;
+        this.initOK = loadFile("config.ini");
         System.out.println("Hello from Init");
     }
-    
+    private boolean loadFile(String fileName){
+        boolean retVal = false;
+        InputStream input = this.getClass().getResourceAsStream(fileName);
+        if(null == input)
+            System.out.println("Init file missing fatal error");
+        else
+            retVal = true;
+        return retVal;
+    }
+            
 }
