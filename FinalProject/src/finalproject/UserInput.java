4c5692dca921361f5b743d6532d0dd104bfe34f0
Beto <evol999@hotmail.com>
2021-04-29 16:26:03 -0500

    Skeleton parts added

--- FinalProject/src/finalproject/UserInput.java
+++ FinalProject/src/finalproject/UserInput.java
@@ -5,6 +5,8 @@
  */
 package finalproject;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author evol9
@@ -16,12 +18,16 @@ class UserInput {
         menuUser.setText("Are you a Visitor or a Patient?");
         menuUser.setOption("Patient");  //  1.
         menuUser.setOption("Visitor");  //  2.
+        menuUser.setOption("Administrator");  //  3.
         menuUser.run();
         if (1 == menuUser.getSelection()) {
             processUser();
         }
         if (2 == menuUser.getSelection()) {
             processVisitor();
+        }
+        if (3 == menuUser.getSelection()) {
+            processAdmin();
         }
     }
 
@@ -53,10 +59,38 @@ class UserInput {
         menuUser.setText(greeting);
         menuUser.setOption("Book appointment by area of expertise");  //  1.
         menuUser.setOption("Book appointment by physician");  //  2.
+        menuUser.setOption("Cancel appointment");  //  3.
+        menuUser.setOption("Attend appointment");  //  4.
+        menuUser.setOption("Report missing appointment");  //  4.
         menuUser.run();
+        if (1 == menuUser.getSelection()) {
+            bookByArea();
+        }
+        if (2 == menuUser.getSelection()) {
+            bookByPhysician();
+        }
+
     }
 
     private void registerUser() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void bookByArea() {
+        Menu menuUser = new Menu();
+        ArrayList<String> Areas = new ArrayList<>();
+
+        menuUser.setText("");
+        menuUser.setOption("Yes");  //  1.
+        menuUser.setOption("No");  //  2.
+        menuUser.run();
+    }
+
+    private void bookByPhysician() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    private void processAdmin() {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 }
